75b692085475f071ce7a65d46f288ef2ee338bd8
FerreiraVGabriel <46385099+FerreiraVGabriel@users.noreply.github.com>
2025-07-09 13:55:21 -0300

    Ajuste de tradução

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,25 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+// app.config.ts ou no AppComponent
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+export const appConfig = {
+  providers: [
+    provideHttpClient(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    )
+  ]
 };
--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -3,6 +3,8 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon'; 
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { ThemeService } from '../../../services/theme.service';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import {MatSelectModule} from '@angular/material/select';
 
 @Component({
   selector: 'app-nav-bar',
@@ -10,7 +12,9 @@ import { ThemeService } from '../../../services/theme.service';
   imports: [
     CommonModule,
     MatMenuModule,
-    MatIconModule
+    MatIconModule,
+    TranslateModule,
+    MatSelectModule
   ],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
@@ -18,11 +22,21 @@ import { ThemeService } from '../../../services/theme.service';
 export class NavBarComponent implements OnInit {
   isNavbarCollapsed = true;
   isDarkTheme = false;
+  languages = [
+    { label: 'Português', value: 'pt', flag: 'assets/img/br.png' },
+    { label: 'English', value: 'en', flag: 'assets/img/usa.png' }
+  ];
 
-  constructor(
+  constructor( public translate: TranslateService,
     private themeService: ThemeService,
     @Inject(PLATFORM_ID) private platformId: Object
-  ) {}
+  ) {
+    translate.addLangs(['en', 'pt']);
+    translate.setDefaultLang('pt');
+
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang?.match(/en|pt/) ? browserLang : 'pt');
+  }
 
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
@@ -39,6 +53,20 @@ export class NavBarComponent implements OnInit {
     this.themeService.toggleTheme();
     this.isDarkTheme = this.themeService.currentTheme === 'dark';
   }
+
+  changeLanguage(lang: string) {
+    this.translate.use(lang);
+  }
+
+
+  getFlag(lang: string): string {
+    return this.languages.find(l => l.value === lang)?.flag || '';
+  }
+
+  getLabel(lang: string): string {
+    return this.languages.find(l => l.value === lang)?.label || '';
+  }
+
 
   // changeLanguage(event: Event) {
   // const selectElement = event.target as HTMLSelectElement;
